799f5d00b5bddb439070e15c38f97fa5624ced6d
Danaa4 <Danaa4@users.noreply.github.com>
2022-09-02 11:43:20 +0300

    .

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import React,{useState} from 'react'
 
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState(['Latte','Mocha'])
+  const[drink, setDrink] =useState('')
+
+  function changed(x){
+    setDrink(x.target.value)
+
+   }
+
+  function addItem(){
+    let temp=[];
+    data.map(x => temp.push(x));
+    temp.push(drink);
+    setData(temp);
+
+    //setData([...data])
+
+  }
 
   return <div className="App">
     <div className="container">
       <h3>My Items</h3>
-      <input />
-      <button>Add Item!</button>
+      <input type='text' value={drink} onChange={changed}  />
+      <button onClick={addItem} >Add Item!</button>
+      
       <div className="items-list">
-        <p>Latte</p>
-        <p>Mocha</p>
+        {data.map(x => <p>{x}</p>)}
       </div>
     </div>
   </div>;
